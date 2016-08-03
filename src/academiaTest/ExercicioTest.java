89380f552d953d868945b3fdfdb76b0a8adb46fe
damiaord <damiaord@lcc2-03.lcc.ufcg.edu.br>
2016-08-03 14:53:49 -0300

    Primeiro envio lab05

---
+++ src/academiaTest/ExercicioTest.java
@@ -0,0 +1,34 @@
+package academiaTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExercicioTest {
+
+	@Test
+	public void testExercicio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCph() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalPerHoras() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
